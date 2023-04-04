e4f63072a6016a4ccd902ad8a13b7ee231bcfc25
eunrain <113877276+eunrain@users.noreply.github.com>
2023-04-05 00:24:20 +0900

    [level 0] Title: 로그인 성공?, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -3,7 +3,7 @@ function solution(id_pw, db) {
     db.map((user) => {
     if(user[0] === id_pw[0] && user[1] === id_pw[1]) answer ="login"
     if(user[0] === id_pw[0] && user[1] !== id_pw[1]) answer = "wrong pw"
-    if(user[0] !== id_pw[0] && user[1] !== id_pw[1]) return answer="fail"
+    if(user[0] !== id_pw[0] && user[1] !== id_pw[1]) answer="fail"
     })
    
     return answer;
