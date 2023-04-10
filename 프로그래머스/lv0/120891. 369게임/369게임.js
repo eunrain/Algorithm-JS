101e8c5f23cbaefb8eca1ed26fa98800bb8be66e
eunrain <113877276+eunrain@users.noreply.github.com>
2023-04-10 23:01:42 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,7 +1,7 @@
 function solution(order) {
       let claps = 0;
       const arr = [...order.toString()];
-      for (let i = 0; i <= arr.length; i++) {
+      for (let i = 0; i < arr.length; i++) {
         if (arr[i] === "3" || arr[i] === "6" || arr[i] === "9") claps++;
       }
       return claps;
