d57cb938952cc4d58e7fb2d0607a361792ffac1f
eunrain <113877276+eunrain@users.noreply.github.com>
2023-04-20 17:22:17 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -6,16 +6,13 @@ function solution(keyinput, board) {
         if (keyinput[i] === "right") result[0]++;
         if (keyinput[i] === "up") result[1]++;
         if (keyinput[i] === "down") result[1]--;
-          
-        // 결과값이 범위를 벗어나는 경우 보정
-        if (Math.abs(result[0]) > Math.floor(x / 2))
-            result[0] < 0
-            ? (result[0] = -Math.floor(x / 2))
-            : (result[0] = Math.floor(x / 2));
-        if (Math.abs(result[1]) > Math.floor(y / 2))
-            result[1] < 0
-            ? (result[1] = -Math.floor(y / 2))
-            : (result[1] = Math.floor(y / 2));
+      }
+    
+        if (Math.abs(result[0]) > Math.floor(x / 2)) {
+        result[0] = result[0] > 0 ? Math.floor(x / 2) : Math.ceil(-(x - 1) / 2);
+      }
+        if (Math.abs(result[1]) > Math.floor(y / 2)) {
+        result[1] = result[1] > 0 ? Math.floor(y / 2) : Math.ceil(-(y - 1) / 2);
       }
     
       return result;
